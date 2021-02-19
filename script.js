7eecc0c3087fb267fba4bba9971181c94783d7ff
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-02-19 19:22:50 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const account1 = {
   owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300, 654, 489, 54, -985],
   interestRate: 1.2, // %
   pin: 1111,
 };
@@ -53,3 +53,21 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const html = ` <div class="movements">
+        <div class="movements__row"> 
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov} $</div>
+        </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
