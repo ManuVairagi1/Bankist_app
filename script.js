c36991fb6c3f0620fa1f85a0dc52bddf8183dc36
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-03-02 13:19:40 +0530

    commit

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ btnClose.addEventListener("click", function (e) {
   inputCloseUsername.value = inputClosePin.value = "";
 });
 
-//lectures, not the part of application that we are building. 😀😀
+///lectures, not the part of application that we are building. 😀😀
 //CODING CHALLANGE
 /* 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
